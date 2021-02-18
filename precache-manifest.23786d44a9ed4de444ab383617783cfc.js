0b03f21a694a55e47f123fb32cfea32ef1de758c
prinzo <brunoprinzo@hotmail.com>
2021-02-18 14:54:57 -0300

    Updates

---
+++ precache-manifest.23786d44a9ed4de444ab383617783cfc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624dd2e4eafb830e41af0b205ea04207",
+    "url": "/recipApp/index.html"
+  },
+  {
+    "revision": "f10353546b056857f98c",
+    "url": "/recipApp/static/css/main.18fa1cd8.chunk.css"
+  },
+  {
+    "revision": "a908bde47185369b71ef",
+    "url": "/recipApp/static/js/2.abc86158.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipApp/static/js/2.abc86158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10353546b056857f98c",
+    "url": "/recipApp/static/js/main.7eb2949e.chunk.js"
+  },
+  {
+    "revision": "6c13871d508bdae2d624",
+    "url": "/recipApp/static/js/runtime-main.87b9dcf8.js"
+  },
+  {
+    "revision": "5159a831406ec092dfc12c0a00324987",
+    "url": "/recipApp/static/media/hamburger.5159a831.svg"
+  },
+  {
+    "revision": "69b6de54a226e800e6251cce0e8bf28d",
+    "url": "/recipApp/static/media/macarrao.69b6de54.jpg"
+  }
+]);
